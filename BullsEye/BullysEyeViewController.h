1c07089e657cf56835fd323600d1eddce30db9ff
Rene Kurfürst <rkurfuerst@ruesselair.fritz.box>
2012-11-09 17:47:54 +0100

    Initial Commit

---
+++ BullsEye/BullysEyeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BullysEyeViewController.h
+//  BullsEye
+//
+//  Created by Rene Kurfürst on 09.11.12.
+//  Copyright (c) 2012 Rene Kurfürst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BullysEyeViewController : UIViewController
+
+@end
---
+++ BullsEye/BullysEyeViewController.m
@@ -0,0 +1,29 @@
+//
+//  BullysEyeViewController.m
+//  BullsEye
+//
+//  Created by Rene Kurfürst on 09.11.12.
+//  Copyright (c) 2012 Rene Kurfürst. All rights reserved.
+//
+
+#import "BullysEyeViewController.h"
+
+@interface BullysEyeViewController ()
+
+@end
+
+@implementation BullysEyeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
